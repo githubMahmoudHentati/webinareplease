ef21a51bac1b9454f461f8abd3e280161296559c
Taha Amaidi Sastec <taha.amaidi@sastec-group.com>
2021-06-01 18:05:48 +0000

    WS: #0019057 : Header APP

--- src/formDirectVideo/components/modalSpeacker.js
+++
@@ -1,8 +0,0 @@
-import React, { useState,useEffect,useRef } from 'react';
-import {Row,Col,Input,Button,Modal} from 'antd'
-import '../formDirectVideo.scss'
-import {MailOutlined} from '@ant-design/icons';
-
- export const ModalSpeacker =()=>{
-     
- }
