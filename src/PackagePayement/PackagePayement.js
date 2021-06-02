44d44b234ea2fa254129f62beae0bd39b7870185
Taha Amaidi Sastec <taha.amaidi@sastec-group.com>
2021-06-02 15:30:10 +0000

    WS:#0019069: Choice package webinairePlease

--- src/PackagePayement/PackagePayement.js
+++ src/PackagePayement/PackagePayement.js
@@ -1,12 +1,13 @@
 import React,{useState , useEffect} from 'react';
 import WebinairePlease from "../utils/components/WebinairePlease";
+import ChoicePackage from "./components/ChoicePackage";
 
 function PackagePayement(){
 
     return(
         <div className="PackagePayement">
           <WebinairePlease>
-
+            <ChoicePackage/>
           </WebinairePlease>
         </div>
     )
