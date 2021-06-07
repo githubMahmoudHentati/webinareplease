d3bedcdead150a1636db1ac6b75decb1f1ec6445
Mahmoud Hentati <mahmoud.henteti@sastec-group.com>
2021-06-07 18:41:11 +0000

    socailtools

---
+++ src/formDirectVideo/components/socialTools.js
@@ -0,0 +1,230 @@
+import React, { useState,useEffect,useRef } from 'react';
+import {Row,Col,Switch,List,DatePicker,Button} from 'antd'
+import {DownOutlined,CloseOutlined,PlusOutlined,FacebookFilled,GatewayOutlined} from '@ant-design/icons';
+import empreint from "../../assets/logo-empreinte.svg"
+import fb from "../../assets/fb.svg"
+
+
+export const SocialTools=()=>{
+    const [typePost, setTypePost] = useState([
+        {
+            id:0,
+            type:"Facebook post",
+            switch :true,
+            logo:"",
+            plan:[{active:true,startDate:"",endDate:""}]
+        },
+        {
+            id:1,
+            type:"Youtube post",
+            switch :false,
+            logo:"",
+            plan:[{active:true,startDate:"",endDate:""}]
+        },
+        {
+            id:2,
+            type:"Linkedlin post",
+            switch :false,
+            logo:"",
+            plan:[{active:true,startDate:"",endDate:""}]
+        },
+        {
+            id:3,
+            type:"Twitter post",
+            switch :false,
+            logo:"",
+            plan:[{active:true,startDate:"",endDate:""}]
+        },
+        {
+            id:4,
+            type:"Instagram post",
+            switch :false,
+            logo:"",
+            plan:[{active:true,startDate:"",endDate:""}]
+        }
+    ]);
+    const addPlan =(index)=>{
+        setTypePost(typePost.map(el => (el.id === index ? {...el.plan,
+            id:index,
+            type:el.type,
+            switch :el.switch,
+            logo:el.logo,
+            plan: [...el.plan,{active:true,startDate:"",endDate:""}]
+         } : el)))
+    }
+    const activePost=(checked,index)=>{
+        setTypePost(typePost.map(el => (el.id === index ? {...el, switch:checked} : el)))
+    }
+
+    const activePlan=(indexPost,indexPlan)=>{
+        (typePost.map((element,index)=>{index===indexPost&&element[index].map((e,i)=>{
+            indexPlan===i?
+            setTypePost({...e,
+                id:index,
+                type:element.type,
+                switch :element.switch,
+                logo:element.logo,
+            }):
+                setTypePost({...e,active:false,startDate:"",endDate:""})
+        })
+    }))
+    }
+    console.log("typePost",typePost)
+
+    return(
+        <Row  gutter={[0, 25]}>
+            <Col span={24}>
+            </Col>
+            <Col span={24}>
+
+                <List
+
+                    dataSource={typePost}
+                    renderItem={(item,index) => (
+                        <List.Item style={{width: "100%", display: "flex", flexDirection: "row"}}>
+                            <Row gutter={[0, 20]} style={{borderBottom: '1px solid RGBA(0, 0, 0, 0.15)',width:"100%",marginBottom: "2%"}}>
+                                <Col span={24}>
+                                    <Row justify={"space-between"}>
+                                        <Col>
+                                            <span style={{
+                                                textAlign: 'left',
+                                                fontSize: "14px",
+                                                fontFamily: "system-ui",
+                                                fontWeight: "500"
+                                            }}>{item.type}</span>
+                                        </Col>
+                                        <Col>
+                                            <Switch onChange={(checked)=>activePost(checked,index)} checked={item.switch}/>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                {item.switch === true &&
+                                <Col style={{marginBottom: "2%"}} span={24}>
+                                    <Row justify={"space-between"}>
+                                        <Col span={8}>
+                                            <Row gutter={[0, 20]}>
+                                                <Col span={24}>
+                                                    <span style={{fontWeight: "400"}}>Planifier  la publication</span>
+                                                </Col>
+                                                {typePost[index].plan.map((element, indexPlan) => {
+                                                        return (
+                                                            <Col className={"col-planification"} span={24}>
+                                                                <Row gutter={[0, 20]}>
+                                                                    <Col span={24}>
+                                                                        <Row justify={"space-between"}>
+                                                                            <Col span={12}>
+                                                                                <Row>
+                                                                                    <Col>
+                                                                                        <DownOutlined onClick={()=>{activePlan(index,indexPlan)}}/>
+                                                                                    </Col>
+                                                                                    <Col offset={2}>
+                                                                                        <span> Planification {indexPlan +1}</span>
+                                                                                    </Col>
+                                                                                </Row>
+                                                                            </Col>
+                                                                            <Col>
+                                                                                <CloseOutlined/>
+                                                                            </Col>
+                                                                        </Row>
+                                                                    </Col>
+                                                                    {element.active &&
+                                                                    <Col span={24}>
+                                                                        <Row gutter={[0, 10]}>
+                                                                            <Col span={24}>
+                                                                                <span>Date de début</span>
+                                                                            </Col>
+                                                                            <Col span={24}>
+                                                                                <DatePicker style={{width: "100%"}}/>
+                                                                            </Col>
+                                                                        </Row>
+                                                                    </Col>
+                                                                    }
+                                                                    {element.active &&
+                                                                    <Col span={24}>
+                                                                        <Row gutter={[0, 10]}>
+                                                                            <Col span={24}>
+                                                                                <span>Date de fin</span>
+                                                                            </Col>
+                                                                            <Col span={24}>
+                                                                                <DatePicker style={{width: "100%"}}/>
+                                                                            </Col>
+                                                                        </Row>
+                                                                    </Col>
+                                                                    }
+                                                                </Row>
+                                                            </Col>
+                                                        )
+                                                    }
+                                                )}
+                                                <Col style={{
+                                                    border: "1px solid RGBA(0, 0, 0, 0.15)",
+                                                    display: "flex",
+                                                    alignItems: "center",
+                                                    justifyContent: "center"
+                                                }} span={24}>
+                                                    <Button  style={{width:"100%"}}onClick={() => addPlan(index)} icon={<PlusOutlined/>}>Ajouter une
+                                                        planification</Button>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                        <Col span={14} >
+                                            <Row gutter={[0, 10]} style={{border: "1px solid RGBA(0, 0, 0, 0.15)", padding: "2%"}}>
+                                                <Col span={24}>
+                                                    <Row justify={"space-between"}>
+                                                        <Col span={12}>
+                                                            <Row justify={"start"}
+                                                                 style={{display: "flex", alignItems: "center"}}>
+                                                                <Col>
+                                                                    <img style={{width: "24px", height: "24px"}}
+                                                                         src={empreint}/>
+                                                                </Col>
+                                                                <Col offset={1}>
+                                                                    <span style={{
+                                                                        textAlign: 'left',
+                                                                        fontSize: "13px",
+                                                                        fontFamily: "system-ui"
+                                                                    }}>Empreinte</span><br/><span style={{
+                                                                    color: "RGBA(0, 0, 0, 0.25)",
+                                                                    fontSize: "11px"
+                                                                }}>Environ 1mn</span>
+                                                                </Col>
+                                                            </Row>
+                                                        </Col>
+                                                        <Col>
+                                                            <img style={{height: "20px", width: "20px"}} src={fb}/>
+                                                        </Col>
+                                                    </Row>
+                                                </Col>
+                                                <Col span={24}>
+                                                    Torquem detrexit hosti exercirlus quid ex ea voluptate et accurate
+                                                    disseredum.
+                                                </Col>
+                                                <Col span={24} style={{display: "flex", justifyContent: "center"}}>
+                                                    <img
+                                                        src={"//test-tv.webtv-solution.com/web/data/vignettes/logoSAB2NjY3ZTg5MmItNGI0MS00YjIyLTgwZGYtNmNkY2NjZTRhZTVishutterstock562442005.jpg"}
+                                                        style={{height: "198px", width: "412.79"}}/>
+                                                </Col>
+                                                <Col span={24}>
+                                                    <span style={{
+                                                        textAlign: 'left',
+                                                        fontSize: "13px",
+                                                        fontFamily: "system-ui"
+                                                    }}>Fundamentals of Webinar</span><br/>
+                                                    <span style={{
+                                                        color: "RGBA(0, 0, 0, 0.25)",
+                                                        fontSize: "11px"
+                                                    }}>Empriente.com</span>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                }
+                            </Row>
+                        </List.Item>
+                    )}
+                />
+            </Col>
+        </Row>
+    )
+}
