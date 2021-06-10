cadbfcd24357a521d3d680aaf9896ccc8b696c50
Mahmoud Hentati <mahmoud.henteti@sastec-group.com>
2021-06-10 15:27:48 +0000

    Correction style des iframes dans le menu media directe

--- src/utils/components/principalPage.js
+++ src/utils/components/principalPage.js
@@ -11,13 +11,14 @@ import {PasswordEdit} from "../../compteSettings/components/passwordEdit";
 
 export const PrincipalPage =(props)=>{
     const accountMenu = useSelector((state)=>state.Reducer.accountMenu)
+    const directMenu = useSelector((state)=>state.Reducer.directMenu)
     // use Selector redux
     const darkMode = useSelector((state)=> state.Reducer.DarkMode)
     console.log("darmode principalpage",darkMode)
+    const iframeStyle = props.menuType==="accountSetting"?{height:accountMenu===2||accountMenu===1?"100vh":"100%"}:{height:directMenu===2?"100vh":"100%"}
 
     return(
-
-        <div className="showVideosDiv" style={{height:accountMenu===2||accountMenu===1?"100vh":"100%"}}>
+        <div className="showVideosDiv" style={iframeStyle}>
             <GlobalHeader/>
             <Card  style={{height:"93%" , backgroundColor:darkMode===false?"#F0F1F4":"#000000" , border:darkMode===false? "1px solid white": "1px solid #141414" }} className={"card"} >
                 <Card style={{height:"100%" , backgroundColor:darkMode===false?"#FFFFFF":"#141414" , border:darkMode===false? "1px solid white": "1px solid #141414" }}>
