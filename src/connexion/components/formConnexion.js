af0ae96bf6fcc3463c73e1b45ce47d7585d3fd24
Mahmoud Hentati <mahmoud.henteti@sastec-group.com>
2021-06-17 13:11:26 +0000

    Manipuler les champs requis de l'interface connexion

--- src/connexion/components/formConnexion.js
+++ src/connexion/components/formConnexion.js
@@ -1,12 +1,28 @@
 import React, { useState,useEffect,useRef } from 'react';
-import {Row,Col,Input,Button,Card,Tabs,Breadcrumb,Menu,Checkbox} from 'antd'
+import {Row, Col, Input, Button, Card, Tabs, Breadcrumb, Menu, Checkbox, Form} from 'antd'
 import '../connexion.scss'
 import {UserOutlined,UnlockOutlined,EyeTwoTone,EyeInvisibleOutlined} from '@ant-design/icons';
 import {useHistory} from 'react-router-dom';
+import {Hooks} from "../utils/hooks";
 
 export const FormConnexion =()=>{
+    const [form] = Form.useForm();
     const history = useHistory()
+
+    const isValidPassword = (password) => {
+
+        return /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&()^_!"#$%&'*+£,-./:;{}<>=|~?·•¯‾|¦‌‍†‡§¶©®™&@/\◊♠♣♥♦←↑→↓↔áÁâÂàÀåÅãÃäÄæÆçÇéÉêÊèÈëËíÍîÎìÌïÏñÑóÓôÔòÒøØõÕöÖœŒšŠßðÐÞúÚûÛùÙüÜýÝÿŸ¤€$¢£¥ƒαΑβΒγΓδΔεΕζΖηΗθΘιΙκΚλΛμΜνΝξΞοΟπΠρΡσςΣτΤυΥφΦχΧψΨωΩ°µ < >≤≥=≈≠≡±−+×÷⁄%‰¼½¾¹²³ºªƒ″∂∏∑√∞¬∩∫])[A-Za-z\d@$!%*?&()^_!"#$%&'*+£,-./:;{}<>=|~?·•¯‾_ |¦‌‍†‡§¶©®™&@/\◊♠♣♥♦←↑→↓↔áÁâÂàÀåÅãÃäÄæÆçÇéÉêÊèÈëËíÍîÎìÌïÏñÑóÓôÔòÒøØõÕöÖœŒšŠßðÐÞúÚûÛùÙüÜýÝÿŸ¤€$¢£¥ƒαΑβΒγΓδΔεΕζΖηΗθΘιΙκΚλΛμΜνΝξΞοΟπΠρΡσςΣτΤυΥφΦχΧψΨωΩ°µ < >≤≥=≈≠≡±−+×÷⁄%‰¼½¾¹²³ºªƒ″∂∏∑√∞¬∩∫]{8,}$/.test(password)
+    }
+    const requiredFieldRule = [{required: true, message: 'Champs requis'}];
+    const{handleSubmit,values}=Hooks()
+
     return(
+        <Form
+            form={form}
+            layout="horizontal"
+            name="product-form"
+            onFinish={handleSubmit}
+        >
         <Row gutter={[0, 40]} className={'col-connexion'}>
             <Col span={24}>
                 <span className={"span_connexion"}>Connexion</span>
@@ -15,15 +31,37 @@ export const FormConnexion =()=>{
                 <Row gutter={[0, 20]} >
 
                     <Col span={24} className={"col_input"}>
+                        <Form.Item name="email" className={"form-item-style"}
+                                   rules={requiredFieldRule}
+                        >
                         <Input placeholder="default size" prefix={<UserOutlined/>}/>
+                        </Form.Item>
                     </Col>
-
                     <Col span={24} className={"col_input"}>
-                        <Input.Password
-                            placeholder="input password"
-                            iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
-                            prefix={<UnlockOutlined/>}
-                        />
+                        <Form.Item
+                            className={"form-item-style"}
+                            name="password"
+                            rules={[{
+                                required: true,
+                                message: "S'il vous plaît saisissez votre mot de passe!"
+                            },
+                                ({getFieldValue}) => ({
+                                    validator(_, value) {
+                                        if (isValidPassword(values.passwordIntranet)) {
+                                            return Promise.resolve('value');
+                                        }
+                                        return Promise.reject('Minimum 8 caractéres avec au moins une majiscule, un chiffre et un caractère spéciale');
+                                    },
+                                }),
+                            ]}
+                        >
+                            <Input.Password
+                                className={"spn2"}
+                                name="password"
+                                placeholder="Mot de passe"
+                                iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
+                            />
+                        </Form.Item>
                     </Col>
                     <Col span={24}>
                         <Row justify="space-between">
@@ -40,7 +78,7 @@ export const FormConnexion =()=>{
             <Col span={24}>
                 <Row gutter={[20, 20]} >
                     <Col span={24}>
-                        <Button className={"spn_chbx"} style={{width:"100%"}}type="primary">Connexion</Button>
+                        <Button className={"spn_chbx"} style={{width:"100%"}}type="primary" htmlType="submit">Connexion</Button>
                     </Col>
                     <Col >
                         <span className={"spn_chbx"}>Pas encore membre?</span>
@@ -51,5 +89,6 @@ export const FormConnexion =()=>{
                 </Row>
             </Col>
         </Row>
+        </Form>
     )
 }
