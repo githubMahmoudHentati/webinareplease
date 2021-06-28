9aca10d9a8e0204edc429e2dae0a112f6b325fd4
Mahmoud Hentati <mahmoud.henteti@sastec-group.com>
2021-06-28 13:01:03 +0100

    a

---
+++ src/forgetPassword/store/forgetPasswordAction.js
@@ -0,0 +1,28 @@
+
+
+//******************** connexion actions***************//
+
+        export function setForgetPasswordOnchange(e){
+
+            return{
+                type: "SET_ForgetPasswordOnchange",
+                payload:e,
+            }
+        }
+
+//*******************Constraint Data actions*******************//
+
+        export function setForgetPasswordConstraintDataOnchange(e){
+            return{
+                type:"SET_ForgetPasswordConstraintDataOnchange",
+                payload:e
+            }
+        }
+
+
+
+
+
+
+
+
