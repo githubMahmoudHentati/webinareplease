bbd965c9f202be631649fe86414847d3548fc54c
Dell <mahmoud.henteti@sastec-group.com>
2021-08-13 15:12:59 +0100

    a

---
+++ src/formDirectVideo/utils/StatusMessages.js
@@ -0,0 +1,63 @@
+import React,{useState} from 'react';
+import {message} from "antd";
+import i18n from '../../i18n/index';
+import {setConfirmAccountConstraintDataOnchange} from "../../confirmAccount/store/forgetPasswordAction";
+import {useDispatch} from "react-redux";
+import {setFormDirectLiveConstraintDataOnchange} from "../store/formDirectVideoAction";
+
+export const StatusMessages = (id) => {
+    const dispatch = useDispatch()
+
+    const success_submit = async (code) => {
+        dispatch(setFormDirectLiveConstraintDataOnchange({
+            constraintDataNameChange: "leaveToast",
+            constraintDataValueChange: false
+        }))
+        const successMessage = {
+            200: id ? "Le direct est modifié avec success" : "Le direct est crée avec success",
+        }
+        message.success({
+            content: successMessage[code],
+            duration: 2,
+            style: {
+                marginTop: '2vh',
+            },
+        })
+            .then(async () =>
+                dispatch(setFormDirectLiveConstraintDataOnchange({
+                    constraintDataNameChange: "leaveToast",
+                    constraintDataValueChange: true
+                }))
+            )
+    };
+
+    const error_submit = async (code) => {
+        const errorMessage = {
+            400: id ? "Ooops , il y a  un problème qui s'est produit lors la modification" : "Ooops , il y a  un problème qui s'est produit lors la creation",
+        }
+        dispatch(setFormDirectLiveConstraintDataOnchange({
+            constraintDataNameChange: "leaveToast",
+            constraintDataValueChange: false
+        }))
+        message
+            .error({
+                content: errorMessage[code],
+                duration: 2,
+                style: {
+                    marginTop: '2vh',
+                },
+            })
+            .then(async () =>
+                dispatch(setFormDirectLiveConstraintDataOnchange({
+                    constraintDataNameChange: "leaveToast",
+                    constraintDataValueChange: true
+                }))
+            )
+    };
+
+    return ({
+        success_submit,
+        error_submit
+    })
+
+}
