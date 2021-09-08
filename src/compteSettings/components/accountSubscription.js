b88e08228ff0a450a281e4b21789194ae81001fd
Taha Amaidi Sastec <taha.amaidi@sastec-group.com>
2021-09-08 16:41:47 +0000

    [WEBPLEASE-260 , WEBPLEASE-248]:Reviw inegration dark mode

--- src/compteSettings/components/accountSubscription.js
+++ src/compteSettings/components/accountSubscription.js
@@ -74,23 +74,23 @@ export const AccountSubscription=()=>{
                     <Col><img className={"img-strip"} src={strip} alt={""}/></Col>
                     <Col  className={"parag_abonnement"} offset={1} style={{color:darkMode===false?"":"rgba(255, 255, 255, 0.85)" }}> {values.subscription.paymentMode}</Col>
                     <Col span={24}>
-                        <Button className={"parag_abonnement"} disabled style={{background:darkMode===false?"":"#141414" , color:darkMode===false?"":"rgba(255, 255, 255, 0.85)", border:darkMode===false?"":"1px solid rgba(255, 255, 255, 0.15)"}}> {t("CompteSettings.NewPaymentMeth")}</Button>
+                        <Button className={"parag_abonnement"} disabled style={{background:darkMode===false?"":"#141414" , color:darkMode===false?"":"rgba(255, 255, 255, 0.25)", border:darkMode===false?"":"1px solid rgba(255, 255, 255, 0.15)"}}> {t("CompteSettings.NewPaymentMeth")}</Button>
                     </Col>
                 </Row>
             </Col>
             <Col span={24}>
-                <Row gutter={[0, 20]} className={"row_abonnement"}>
+                <Row gutter={[0, 20]} className={"row_abonnement"} >
                     <Col offset={0} span={24}>
                         <Row justify={"space-between"}>
                             <Col  xs={{ span: 24}} sm={{ span: 24}} md={{ span: 14}} lg={{span:14}}>
                                 <span className={"spn_abonnement"} style={{color:darkMode===false?"":"rgba(255, 255, 255, 0.85)"}}>{t("CompteSettings.SubscriptionBill")}</span>
                             </Col>
                             <Col className={"col_update_infos"} xs={{ span: 24}} sm={{ span: 24}} md={{ span: 10}} lg={{span:10}} >
-                                <Button className={"parag_abonnement"} disabled style={{background:darkMode===false?"":"#141414" , color:darkMode===false?"":"rgba(255, 255, 255, 0.85)", border:darkMode===false?"":"1px solid rgba(255, 255, 255, 0.15)"}}> {t("CompteSettings.ChangeBillInfos")}</Button>
+                                <Button className={"parag_abonnement"} disabled style={{background:darkMode===false?"":"#141414" , color:darkMode===false?"":"rgba(255, 255, 255, 0.25)", border:darkMode===false?"":"1px solid rgba(255, 255, 255, 0.15)"}}> {t("CompteSettings.ChangeBillInfos")}</Button>
                             </Col>
                         </Row>
                     </Col>
-                    <Col   xs={{ span: 23}} sm={{ span: 23}} md={{ span: 24}} lg={{span:24}} >
+                    <Col  xs={{ span: 23}} sm={{ span: 23}} md={{ span: 24}} lg={{span:24}} >
                         <SubscriptionTable/>
                     </Col>
                 </Row>
