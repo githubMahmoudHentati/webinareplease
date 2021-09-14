825a8253293547fe17062b684df8a0de9a3243d2
m.kharrat <maha.kharrat@sastec-group.com>
2021-09-14 14:01:42 +0200

    fix redirect login page + fix get thumbnail + fix get questions

--- src/utils/grapqhQL/graphQLFetchData.js
+++ src/utils/grapqhQL/graphQLFetchData.js
@@ -1,16 +1,14 @@
 import {useLazyQuery, useQuery} from "@apollo/react-hooks";
 import {graphQL_shema} from "./shemaGraphQL";
 import {useDispatch} from "react-redux";
-import {setAppSetLogin} from "../redux/actions";
+import {setAppSetLogin, setAppSetLogout} from "../redux/actions";
 import {useState} from "react";
 import {useHistory} from 'react-router-dom';
 
 
 export const GraphQLFetchData=(storageData)=> {
     const dispatch = useDispatch()
-    const history = useHistory();
     // Read Data from Hooks
-
     // use Query to fetch Data
     const [verificationToken, setVerificationToken] = useState(false);
     const token = localStorage.getItem('jwtToken')?localStorage.getItem('jwtToken'):'';
@@ -27,8 +25,8 @@ export const GraphQLFetchData=(storageData)=> {
             if (data.tokenverification.code===200)
             {
                 dispatch(setAppSetLogin(token));
-            } else {
-                history.push("/connexion")
+            }else{
+                dispatch(setAppSetLogout());
             }
             setVerificationToken(true)
         }
